95117210f603acc70558e3b175a0195fe0ff3451
Rishabh <rishabh@example.com>
2024-07-22 18:54:41 +0530

    updated academic year creation

--- tests/Academic.spec.js
+++ tests/Academic.spec.js
@@ -3,20 +3,19 @@ import { test, expect } from '@playwright/test';
 test.describe.serial('test for academic year creation and deletion ', () => {
 
     test('test for academic year creation', async ({ page }) => {
-        await page.goto('https://testschool.launchmysite.in/');
-        await page.getByPlaceholder('Enter your Email').click();
-        await page.getByPlaceholder('Enter your Email').fill('test@example.com');
-        await page.getByPlaceholder('Enter your password').click();
-        await page.getByPlaceholder('Enter your password').fill('test@123');
-        await page.getByRole('button', { name: 'Sign In' }).click();
-        await page.locator('div:nth-child(8)').click();
-        await page.getByPlaceholder('Enter start year').fill('2027');
-        await page.locator('div').filter({ hasText: /^End Year\*$/ }).locator('div').nth(2).click();
-        await page.getByPlaceholder('Enter end year').fill('2028');
-        await page.getByRole('button', { name: 'Create' }).click();
-        await expect(page.getByText('Academic Year Created Successfully!')).toBeVisible(); 
-        await page.goto('https://testschool.launchmysite.in/academicYear');
-        await expect(page.getByText('AY-2027-2028').nth(0)).toBeVisible();
+      await page.goto('https://testschool.launchmysite.in/');
+      await page.getByPlaceholder('Enter your Email').click();
+      await page.getByPlaceholder('Enter your Email').fill('test@example.com');
+      await page.getByPlaceholder('Enter your password').click();
+      await page.getByPlaceholder('Enter your password').fill('test@123');
+      await page.getByRole('button', { name: 'Sign In' }).click();
+      await page.locator('div:nth-child(8)').click();
+      await page.getByPlaceholder('Enter start year').click();
+      await page.getByPlaceholder('Enter start year').fill('2027');
+      await page.getByPlaceholder('Enter end year').click();
+      await page.getByPlaceholder('Enter end year').fill('2028');
+      await page.getByRole('button', { name: 'Create' }).click();
+      await expect(page.getByText('Academic Year Created Successfully!')).toBeVisible();
       });
 
 
@@ -42,3 +41,5 @@ test.describe.serial('test for academic year creation and deletion ', () => {
 
 
 });
+
+
