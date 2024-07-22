927ada8934f96a3774f0301316d25bba349f99c7
Rishabh <rishabh@example.com>
2024-07-22 20:37:59 +0530

    added test id in the test cases

--- tests/Academic.spec.js
+++ tests/Academic.spec.js
@@ -31,14 +31,12 @@ test.describe.serial('test for academic year creation and deletion ', () => {
       await page.getByPlaceholder('Enter your password').fill('test@123');
       await page.getByRole('button', { name: 'Sign In' }).click();
       await page.locator('div:nth-child(8)').click();
-      await page.getByPlaceholder('Enter start year', { timeout: 60000 }).click();
-      // await page.getByPlaceholder('Enter start year').fill('2027');
-      await page.getByPlaceholder('Enter start year').fill('2027');
-      await page.getByPlaceholder('Enter end year').click();
-      await page.getByPlaceholder('Enter end year').fill('2028');
+      await page.getByTestId('start-year').fill('2027');
+      await page.getByTestId('end-year').fill('2028');
       await page.getByRole('button', { name: 'Create' }).click();
       await expect(page.getByText('Academic Year Created Successfully!')).toBeVisible();
       });
+
 
 
 
