2e7bd32fa77f998face08df8be40b94988027f4c
Rishabh <rishabh@example.com>
2024-07-23 20:05:11 +0530

    new test cases for class creation

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -124,3 +124,107 @@ test('test for student admissions1', async ({ page }) => {
     await page.getByRole('button', { name: 'Submit' }).click();
     // const element = await expect(page.getByText('Student Created!')).toBeVisible();
   });
+
+
+
+
+
+
+/////////////////////////////-------------------===========--------
+
+// test('test for creating a batch', async ({ page }) => {
+//   await page.goto('https://testschool.launchmysite.in/');
+//   await page.getByPlaceholder('Enter your Email').click();
+//   await page.getByPlaceholder('Enter your Email').fill('test@example.com');
+//   await page.getByPlaceholder('Enter your password').click();
+//   await page.getByPlaceholder('Enter your password').fill('test@123');
+//   await page.getByRole('button', { name: 'Sign In' }).click();
+//   await page.locator('div:nth-child(2) > div:nth-child(3)').first().click();
+//   await page.getByRole('link', { name: 'Manage Class' }).click();
+//   await page.getByText('nursery').click();
+//   await page.getByRole('link', { name: 'View Section (1)' }).click();
+//   await page.getByRole('button', { name: 'Add New Section' }).click();
+//   await page.getByPlaceholder('Enter section name').click();
+//   await page.getByPlaceholder('Enter section name').press('CapsLock');
+//   await page.getByPlaceholder('Enter section name').fill('C');
+//   await page.getByPlaceholder('Enter section name').press('CapsLock');
+//   await page.getByPlaceholder('Enter start date').fill('2024-01-01');
+//   await page.getByText('EndDate').click();
+//   await page.getByPlaceholder('Enter end date').fill('2025-01-01');;
+//   await page.getByLabel('Select Academic Year').click();
+//   await page.getByLabel('AY-2025-2026', { exact: true }).press('ArrowDown');
+//   await page.getByLabel('AY-2024-2025', { exact: true }).press('Enter');
+//   await page.getByRole('button', { name: 'Add' }).click();
+//   await page.goto('https://testschool.launchmysite.in/class_and_section/manage-class')
+//   await expect(page.getByText('View Section (3)')).toBeVisible();
+// });
+
+
+
+
+
+// test('test for deleting batches', async ({ page }) => {
+//   await page.goto('https://testschool.launchmysite.in/');
+//   await page.getByPlaceholder('Enter your Email').click();
+//   await page.getByPlaceholder('Enter your Email').fill('test@example.com');
+//   await page.getByPlaceholder('Enter your password').click();
+//   await page.getByPlaceholder('Enter your password').fill('test@123');
+//   await page.getByRole('button', { name: 'Sign In' }).click();
+//   await page.locator('div:nth-child(2) > div:nth-child(3)').first().click();
+//   await page.getByRole('link', { name: 'Manage Class' }).click();
+//   await page.getByText('nursery').click();
+//   await page.getByRole('link', { name: 'View Section (3)' }).click();
+//   await page.getByRole('cell', { name: 'C-' }).click();
+//   await page.getByRole('row', { name: 'Row Actions C-2024 01-01-2024' }).getByLabel('Row Actions').click();
+//   page.once('dialog', dialog => {
+//     console.log(`Dialog message: ${dialog.message()}`);
+//     dialog.accept().catch(() => {});
+//   });
+//   await page.getByRole('menuitem', { name: 'Delete' }).click();
+//   await page.getByRole('cell', { name: 'B-' }).click();
+//   await page.getByRole('row', { name: 'Row Actions B-2024 01-01-2024' }).getByLabel('Row Actions').click();
+//   page.once('dialog', dialog => {
+//     console.log(`Dialog message: ${dialog.message()}`);
+//     dialog.accept().catch(() => {});
+//   });
+//   await page.getByRole('menuitem', { name: 'Delete' }).click();
+//   await page.getByRole('main').locator('div').filter({ hasText: /^Deleted Successfully!$/ }).nth(2).click();
+//   await page.getByRole('cell', { name: 'A-' }).click();
+//   await page.getByLabel('Row Actions').click();
+//   page.once('dialog', dialog => {
+//     console.log(`Dialog message: ${dialog.message()}`);
+//     dialog.accept().catch(() => {});
+//   });
+//   await page.getByRole('menuitem', { name: 'Delete' }).click();
+//   await page.goto('https://testschool.launchmysite.in/class_and_section/manage-class')
+//   await expect(page.getByText('View Section (0)')).toBeVisible();
+// });
+
+
+
+
+
+
+
+// test('test for deleting a class', async ({ page }) => {
+//   await page.goto('https://testschool.launchmysite.in/');
+//   await page.getByPlaceholder('Enter your Email').click();
+//   await page.getByPlaceholder('Enter your Email').fill('test@example.com');
+//   await page.getByPlaceholder('Enter your password').click();
+//   await page.getByPlaceholder('Enter your password').fill('test@123');
+//   await page.getByRole('button', { name: 'Sign In' }).click();
+//   await page.locator('div:nth-child(2) > div:nth-child(3)').first().click();
+//   await page.getByRole('link', { name: 'Manage Class' }).click();
+//   await page.getByText('nursery').click();
+//   page.once('dialog', dialog => {
+//     console.log(`Dialog message: ${dialog.message()}`);
+//     dialog.dismiss().catch(() => {});
+//   });
+//   await page.getByText('Delete').click();
+//   await page.goto('https://testschool.launchmysite.in/class_and_section/manage-class');
+//   await expect(page.getByText('nursery')).not.toBeVisible();
+// });
+
+
+
+
