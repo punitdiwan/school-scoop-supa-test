062f7426bf7f28a52c7722287d961584a5db6135
Rishabh <rishabh@example.com>
2024-07-29 19:56:44 +0530

    added test cases for fee section

--- tests/Transport.spec.js
+++ tests/Transport.spec.js
@@ -22,23 +22,6 @@ test.describe('test for transportation ', () => {
   });
 
 
-test('test for Route Wise Report', async ({ page }) => {
-  await page.goto('https://testschool.launchmysite.in/');
-  await page.locator('.group > div > .inline-flex').first().click();
-  await page.getByPlaceholder('Enter your Email').fill('test@example.com');
-  await page.getByPlaceholder('Enter your password').click();
-  await page.getByPlaceholder('Enter your password').fill('test@123');
-  await page.getByPlaceholder('Enter your password').press('Enter');
-  await page.locator('div:nth-child(7)').first().click();
-  await page.getByRole('link', { name: 'Route Wise Report' }).click();
-  await page.getByText('Route*Select').click();
-  await page.locator('div').filter({ hasText: /^Select RoutelalghatiSelect Route$/ }).first().click();
-  await page.getByLabel('lalghati', { exact: true }).click();
-  await expect(page.getByText('test')).toBeVisible();
-});
-
-
-
 test('test for deleting a route', async ({ page }) => {
   await page.goto('https://testschool.launchmysite.in/');
   await page.getByPlaceholder('Enter your Email').click();
@@ -142,19 +125,21 @@ test('test for transport fee payment', async ({ page }) => {
   await page.getByLabel('class-4th-A-2024', { exact: true }).click();
   await page.getByText('test', { exact: true }).click();
   await page.getByRole('link', { name: 'Pay Fees' }).click();
-  await page.locator('div').filter({ hasText: /^Select a CollectionfirstSelect a Collection$/ }).first().click();
+  await page.locator('div').filter({ hasText: /^Select a CollectionfirstsecondSelect a Collection$/ }).first().click();
   await page.getByLabel('first', { exact: true }).click();
-  await page.getByText('test', { exact: true }).click();
   await page.locator('div').filter({ hasText: /^Select Payment ModeCashUPIOnlineDDChequeSelect Payment Mode$/ }).first().click();
-  // await page.getByLabel('Cash', { exact: true }).getByText('Cash').click();
-  await page.locator('div').filter({ hasText: /^Enter Amount$/ }).first().click();
+  // await page.getByLabel('Cash', { exact: true }).click();
+  await page.getByLabel('Enter Amount').click();
   await page.getByLabel('Enter Amount').fill('10');
   page.once('dialog', dialog => {
     console.log(`Dialog message: ${dialog.message()}`);
-    dialog.accept().catch(() => {});
+    dialog.dismiss().catch(() => {});
   });
   await page.getByRole('button', { name: 'Pay Fees' }).click();
 });
+
+
+
 
 test('test for Unassign Transport', async ({ page }) => {
   await page.goto('https://testschool.launchmysite.in/');
@@ -166,13 +151,12 @@ test('test for Unassign Transport', async ({ page }) => {
   await page.locator('div:nth-child(7)').first().click();
   await page.getByRole('link', { name: 'Unassign Transport' }).click();
   await page.locator('div').filter({ hasText: /^Select Current Courseclass-4thSelect Current Course$/ }).first().click();
-  await page.getByLabel('class-4th', { exact: true }).click();
+  await page.getByLabel('class-4th', { exact: true }).getByText('class-4th').click();
   await page.locator('div').filter({ hasText: /^Select Current Batchclass-4th-A-2024class-4th-B-2024Select Current Batch$/ }).first().click();
-  await page.getByLabel('class-4th-A-2024', { exact: true }).click();
-  await page.locator('div').filter({ hasText: /^Select CollectionfirstSelect Collection$/ }).first().click();
-  await page.getByLabel('first', { exact: true }).click();
+  await page.getByLabel('class-4th-A-2024', { exact: true }).getByText('class-4th-A-').click();
+  await page.locator('div').filter({ hasText: /^Select CollectionfirstsecondSelect Collection$/ }).first().click();
+  await page.getByLabel('first', { exact: true }).getByText('first').click();
   await page.getByRole('button', { name: 'View Student List' }).click();
-  await page.getByText('test', { exact: true }).click();
   await page.getByRole('gridcell', { name: 'Unassign' }).click();
 });
 
@@ -188,16 +172,40 @@ test('test for View All Collection', async ({ page }) => {
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.locator('div:nth-child(7)').first().click();
   await page.getByRole('link', { name: 'View All Collection' }).click();
-  await page.getByLabel('Select a Section').click();
-//   await page.getByLabel('nursery-A-2024', { exact: true }).click();
-//   await page.getByLabel('2', { exact: true }).getByRole('button', { name: 'Edit' }).click();
-//   await page.getByRole('button', { name: 'Update' }).click();
+  await page.getByText('Select Section :Sectionclass-4th-A-2024').click();
+  await page.getByLabel('class-4th-A-2024', { exact: true }).click();
+  await expect(page.getByText('first')).toBeVisible();
 });
 
 
 
 
-test('test for deleting Collection', async ({ page }) => {
+
+test('test for creating fee collection', async ({ page }) => {
+  await page.goto('https://testschool.launchmysite.in/');
+  await page.getByPlaceholder('Enter your Email').click();
+  await page.getByPlaceholder('Enter your Email').fill('test@example.com');
+  await page.getByPlaceholder('Enter your password').click();
+  await page.getByPlaceholder('Enter your password').fill('test@123');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.locator('div:nth-child(7)').first().click();
+  await page.getByRole('link', { name: 'Transport Fees Collection' }).click();
+  await page.getByLabel('Select Period of Collection').click();
+  await page.locator('.mb-4\\.5 > div').click();
+  await page.getByLabel('Select Period of Collection').click();
+  await page.getByLabel('YEARLY', { exact: true }).getByText('YEARLY').click();
+  await page.locator('label').filter({ hasText: 'Select All' }).locator('svg').click();
+  await page.locator('.h-full > .relative').first().click();
+  await page.getByPlaceholder('Enter Fee Collection Name').fill('second');
+  await page.locator('#startDate-1').fill('2024-01-01');
+  await page.locator('#endDate-1').fill('2024-12-01');
+  await page.getByRole('button', { name: 'Create Fee Collection' }).click();
+});
+
+
+
+
+test('test for deleting a fee collection', async ({ page }) => {
   await page.goto('https://testschool.launchmysite.in/');
   await page.getByPlaceholder('Enter your Email').click();
   await page.getByPlaceholder('Enter your Email').fill('test@example.com');
@@ -206,14 +214,17 @@ test('test for deleting Collection', async ({ page }) => {
   await page.getByRole('button', { name: 'Sign In' }).click();
   await page.locator('div:nth-child(7)').first().click();
   await page.getByRole('link', { name: 'View All Collection' }).click();
-  await page.getByLabel('Select a Section').click();
-//   await page.getByLabel('nursery-A-2024', { exact: true }).click();
+  await page.getByText('Select Section :Sectionclass-').click();
+  await page.getByLabel('class-4th-A-2024', { exact: true }).click();
+  await page.getByText('second').click();
   page.once('dialog', dialog => {
     console.log(`Dialog message: ${dialog.message()}`);
     dialog.accept().catch(() => {});
   });
-//   await page.getByLabel('2', { exact: true }).getByRole('button', { name: 'Delete' }).click();
+  await page.getByLabel('2', { exact: true }).getByRole('button', { name: 'Delete' }).click();
 });
+
+
 
 
 
@@ -230,7 +241,7 @@ test('test for viewing Class Batch Wise Report', async ({ page }) => {
   await page.getByLabel('class-4th', { exact: true }).click();
   await page.locator('div').filter({ hasText: /^Select Current Batchclass-4th-A-2024class-4th-B-2024Select Current Batch$/ }).first().click();
   await page.getByLabel('class-4th-A-2024', { exact: true }).click();
-  await page.locator('div').filter({ hasText: /^Select CollectionfirstSelect Collection$/ }).first().click();
+  await page.locator('div').filter({ hasText: /^Select CollectionfirstsecondSelect Collection$/ }).first().click();
   await page.getByLabel('first', { exact: true }).click();
   await page.getByRole('button', { name: 'View Student List' }).click();
       await expect(page.getByText('test', { exact: true })).toBeVisible();
@@ -260,4 +271,19 @@ await expect(page.getByText('new market')).not.toBeVisible();
 
 
 
+});
+
+test('test for Route Wise Report', async ({ page }) => {
+  await page.goto('https://testschool.launchmysite.in/');
+  await page.locator('.group > div > .inline-flex').first().click();
+  await page.getByPlaceholder('Enter your Email').fill('test@example.com');
+  await page.getByPlaceholder('Enter your password').click();
+  await page.getByPlaceholder('Enter your password').fill('test@123');
+  await page.getByPlaceholder('Enter your password').press('Enter');
+  await page.locator('div:nth-child(7)').first().click();
+  await page.getByRole('link', { name: 'Route Wise Report' }).click();
+  await page.getByText('Route*Select').click();
+  await page.locator('div').filter({ hasText: /^Select RoutelalghatiSelect Route$/ }).first().click();
+  await page.getByLabel('lalghati', { exact: true }).click();
+  await expect(page.getByText('test')).toBeVisible();
 });
